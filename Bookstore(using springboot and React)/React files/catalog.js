1b8158a19e612952e61f2b557dd14eaf3cba5ec7
IshaRaghvani <114795191+IshaRaghvani@users.noreply.github.com>
2023-11-29 14:43:40 +0300

    Update catalog.js

--- Bookstore(using springboot and React)/React files/catalog.js
+++ Bookstore(using springboot and React)/React files/catalog.js
@@ -1,1 +1,17 @@
 
+import React from 'react';
+
+const Catalog = ({ books }) => (
+  <div className="catalog">
+    {books.map(book => (
+      <div key={book.id} className="book">
+        
+        <h3>{book.name}</h3>
+        <p>Author: {book.author}</p>
+        <p>Price: ${book.price}</p>
+      </div>
+    ))}
+  </div>
+);
+
+export default Catalog;
